9eac1b80629b87437a9d707ad0ef230c11dfabe3
AshRaven521 <ivan.kondrashov.92@mail.ru>
2023-05-17 18:03:58 +0300

    add model for middle step

---
+++ TestTask/Middleware.cs
@@ -0,0 +1,14 @@
+﻿namespace TestTask
+{
+    public class Middleware
+    {
+        public string Expression { get; set; }
+        public string Operator { get; set; }
+
+        public Middleware(string expr, string oper)
+        {
+            Expression = expr;
+            Operator = oper;
+        }
+    }
+}
