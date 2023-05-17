6faa2b799e53d0229f5f26954c76321d5f8d0ded
AshRaven521 <ivan.kondrashov.92@mail.ru>
2023-05-17 15:21:14 +0300

    task 1 done

--- TestTask/Program.cs
+++ TestTask/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace TestTask
 {
@@ -6,7 +10,48 @@ namespace TestTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TaskOne();
+        }
+
+        static void TaskOne()
+        {
+            string filePath = @"C:\PoliefirAgroTestTask\test.txt";
+
+            string fileData = string.Empty;
+
+            using (var streamReader = new StreamReader(filePath, Encoding.Default))
+            {
+                fileData = streamReader.ReadToEnd();
+            }
+
+            var words = fileData.Trim().Split(' ');
+
+            var results = CountWordsOccurance(words).OrderByDescending(a => a.Value);
+
+            Console.WriteLine("Слово из файла - количество вхождений:\n");
+            foreach (KeyValuePair<string, int> result in results)
+            {
+                Console.WriteLine($"{result.Key} - {result.Value}");
+            }
+        }
+
+        static Dictionary<string, int> CountWordsOccurance(string[] array)
+        {
+            var dict = new Dictionary<string, int>();
+
+            foreach (var arr in array)
+            {
+                int wordCount;
+
+                if(!dict.TryGetValue(arr, out wordCount))
+                {
+                    wordCount = 0;
+                }
+
+                dict[arr] = wordCount + 1;
+            }
+
+            return dict;
         }
     }
 }
